c3ca2a94e52248ad1f9fd4e6927b4f6786346c38
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2025-03-01 23:07:07 -0800

    feat: bottom navigation bar with skeleton pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,74 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
-void main() {
-  runApp(const MyApp());
-}
+import './pages/notification/page.dart';
+import './pages/home/page.dart';
+import './pages/account/page.dart';
+import './pages/search/page.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'QuickBites',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'QuickBites'),
+      home: const NavigationExample(),
     );
   }
 }
 
+class NavigationExample extends StatefulWidget {
+  const NavigationExample({super.key});
+
+  @override
+  State<NavigationExample> createState() => _NavigationExampleState();
+}
+
+class _NavigationExampleState extends State<NavigationExample> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.cyanAccent,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.search_rounded),
+            label: 'Search',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.inbox_rounded)),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person_2_outlined),
+            label: 'Account',
+          ),
+        ],
+      ),
+      body:
+          <Widget>[
+            HomePage(),
+            SearchPage(),
+            NotificationPage(),
+            AccountPage(),
+          ][currentPageIndex],
+    );
+  }
+}
---
+++ lib/pages/search/page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shadowColor: Colors.transparent,
+      margin: const EdgeInsets.all(8.0),
+      child: SizedBox.expand(child: Center(child: Text('Account page'))),
+    );
+  }
+}
