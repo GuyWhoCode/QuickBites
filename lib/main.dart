84580f3ece53c278c8c01e7e6e8cedace7cb80d3
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2025-03-23 16:02:23 -0700

    feat: cloud store skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import './pages/notification/page.dart';
 import './pages/home/page.dart';
@@ -6,10 +7,9 @@ import './pages/search/page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
 
@@ -77,6 +77,23 @@ class _NavBarState extends State<NavBar> {
           ][currentPageIndex],
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          var db = FirebaseFirestore.instance;
+          // Create a new user with a first and last name
+          final user = <String, dynamic>{
+            "first": "Ada",
+            "last": "Lovelace",
+            "born": 1815,
+          };
+
+          // Add a new document with a generated ID
+          db
+              .collection("users")
+              .add(user)
+              .then(
+                (DocumentReference doc) =>
+                    print('DocumentSnapshot added with ID: ${doc.id}'),
+              );
+
           print("Hello world");
         },
         child: const Icon(Icons.add),
