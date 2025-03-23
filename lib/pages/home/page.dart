c4041b73e6a060887c59b680113313cc2bd9aae4
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2025-03-22 22:56:15 -0700

    chore: commit excess code

--- lib/pages/home/page.dart
+++ lib/pages/home/page.dart
@@ -9,82 +9,26 @@ class HomePage extends StatelessWidget {
       shadowColor: Colors.transparent,
       margin: const EdgeInsets.all(8.0),
       child: Scaffold(
-        body: Center(
-          child: Text(
-            'QuickBites',
-            style: Theme.of(context).textTheme.headlineMedium,
+        body: const Center(
+          child: Card(
+            color: Colors.white70,
+            child: Column(
+              children: <Widget>[
+                Image(
+                  image: NetworkImage(
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Jersey_Mike's_logo.svg/1200px-Jersey_Mike's_logo.svg.png",
+                  ),
+                  height: 200.0,
+                  width: 200.0,
+                  fit: BoxFit.contain,
+                ),
+                Text("Jersey Mike's Subs"),
+                Text("4.9 miles away"),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // TRY THIS: Try changing the color here to a specific color (to
-//         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-//         // change color while the other colors stay the same.
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           //
-//           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-//           // action in the IDE, or press "p" in the console), to see the
-//           // wireframe for each widget.
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text('You have pushed the button this many times:'),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
